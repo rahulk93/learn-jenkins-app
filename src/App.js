e2ae33fa2b3adf9b0ecd7bfa6991991ac2bd7329
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2026-02-23 00:19:37 +0000

    Third attempt at changing the name of the webpage!

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning Jenkins
+          Learn Jenkins
         </a>
       </header>
       <p>
